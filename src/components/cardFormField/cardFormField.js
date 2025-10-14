9df5c627b44ef934621383b05c3ac74a62e00cdb
Mator <why101@msn.com>
2025-10-14 15:29:20 -0700

    add names to fields

--- src/components/cardFormField/cardFormField.js
+++ src/components/cardFormField/cardFormField.js
@@ -3,21 +3,29 @@ import Alpine from 'alpinejs';
 const HTMLMap = {
     textarea: field => (
         `<sl-textarea resize="auto" spellcheck="false" 
+                      name="${field.id}"
                       label="${field.displayName}" 
                       x-model="face.${field.id}"></sl-textarea>`
     ),
     select: field => (
-        `<sl-select label="${field.displayName}" x-model="face.${field.id}" x-sl-fix>` +
+        `<sl-select name="${field.id}" 
+                    label="${field.displayName}" 
+                    x-model="face.${field.id}" 
+                    x-sl-fix>` +
             field.options.map(option => (
                 `<sl-option value="${option}">${option}</sl-option>`
             )).join('\n') +
         `</sl-select>`
     ),
     image: field => (
-        `<image-select label="${field.displayName}" x-model="face.${field.id}" :src="face.${field.id}.image" :filename="face.${field.id}.filename"></image-select>`
+        `<image-select label="${field.displayName}"
+                       x-model="face.${field.id}" 
+                       :src="face.${field.id}.image" 
+                       :filename="face.${field.id}.filename"></image-select>`
     ),
     input: field => (
         `<sl-input type="${field.type || 'text'}" 
+                   name="${field.id}"
                    label="${field.displayName}" 
                    x-model="face.${field.id}"></sl-input>`
     )
