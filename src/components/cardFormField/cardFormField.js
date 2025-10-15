9a856a0344c80d6c0266e26f334b3753d261ed8a
Mator <why101@msn.com>
2025-10-15 13:33:21 -0700

    made card form use small inputs

--- src/components/cardFormField/cardFormField.js
+++ src/components/cardFormField/cardFormField.js
@@ -2,32 +2,26 @@ import Alpine from 'alpinejs';
 
 const HTMLMap = {
     textarea: field => (
-        `<sl-textarea resize="auto" spellcheck="false" rows="2"
-                      name="${field.id}"
-                      label="${field.displayName}" 
+        `<sl-textarea resize="auto" spellcheck="false" rows="2" size="small"
+                      name="${field.id}" label="${field.displayName}" 
                       x-model="face.${field.id}"></sl-textarea>`
     ),
     select: field => (
-        `<sl-select name="${field.id}" 
-                    label="${field.displayName}" 
-                    x-model="face.${field.id}" 
-                    x-sl-fix>` +
+        `<sl-select size="small" name="${field.id}" label="${field.displayName}" 
+                    x-model="face.${field.id}" x-sl-fix>` +
             field.options.map(option => (
                 `<sl-option value="${option}">${option}</sl-option>`
             )).join('\n') +
         `</sl-select>`
     ),
     image: field => (
-        `<image-select label="${field.displayName}"
-                       x-model="face.${field.id}" 
-                       :src="face.${field.id}.image" 
+        `<image-select size="small" label="${field.displayName}" 
+                       x-model="face.${field.id}" :src="face.${field.id}.image" 
                        :filename="face.${field.id}.filename"></image-select>`
     ),
     input: field => (
-        `<sl-input type="${field.type || 'text'}" 
-                   name="${field.id}"
-                   label="${field.displayName}" 
-                   x-model="face.${field.id}"></sl-input>`
+        `<sl-input size="small" type="${field.type || 'text'}" name="${field.id}"
+                   label="${field.displayName}" x-model="face.${field.id}"></sl-input>`
     )
 };
 
