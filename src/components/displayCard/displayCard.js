3d622d3f6a0750d0485d4aef5edb629527684cd7
Mator <why101@msn.com>
2025-11-11 17:27:26 -0800

    display card now has scryfall-style turn over button woop

--- src/components/displayCard/displayCard.js
+++ src/components/displayCard/displayCard.js
@@ -4,11 +4,27 @@ import { saveHTMLAsImage } from '../../gfx/imageProcessing';
 import { registerAction } from '../../actionRegistry';
 
 Alpine.data('displayCard', () => ({
+    useFlip: false,
+    flipLabel: 'Turn over',
+
     async init() {
         this.$root.innerHTML = html;
-        this.card = Alpine.store('views').activeCard;
+        this.setupFlip();
         this.bindEvents();
         Alpine.initTree(this.$root);
+    },
+
+    setupFlip() {
+        Alpine.effect(() => {
+            const card = Alpine.store('views').activeCard;
+            this.useFlip = Boolean(card?.model?.front && card?.model?.back);
+            card.useFlip = this.useFlip;
+        });
+    },
+
+    flipCard() {
+        const templateContainer = this.$root.querySelector('.template-container');
+        if (templateContainer) templateContainer.classList.toggle('flipped');
     },
 
     bindEvents() {
