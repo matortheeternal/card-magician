3d6e57c978b6dbfc3f5e14f278b59d42de8e3367
Mator <why101@msn.com>
2025-11-17 17:57:59 -0800

    remove unused alpine directives

--- src/directives/slFix.js
+++
@@ -1,8 +0,0 @@
-import Alpine from 'alpinejs';
-
-Alpine.directive('sl-fix', el => {
-    el.addEventListener('sl-change', () => {
-        const event = new Event('input', { bubbles: true });
-        el.dispatchEvent(event);
-    });
-});
