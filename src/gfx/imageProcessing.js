dc86bd0c0a1b9062479980649f846e0052b2d3be
Mator <why101@msn.com>
2025-11-23 12:35:33 -0800

    use temporary object urls for image processing results

--- src/gfx/imageProcessing.js
+++ src/gfx/imageProcessing.js
@@ -39,16 +39,24 @@ export function loadImage(url) {
     });
 }
 
+const objectUrlMap = new Map();
 const CANVAS_TYPE = 'image/png';
 const CANVAS_QUALITY = 0.92;
-export function writeCanvasToDisk(canvas, fullPath) {
+export function writeCanvasToDisk(canvas, fullPath, cache, localPath) {
     return new Promise((resolve, reject) => {
         canvas.toBlob(async blob => {
             if (!blob) return reject(new Error('Failed to create blob from canvas'));
-            const arrayBuffer = await blob.arrayBuffer();
-            const uint8 = new Uint8Array(arrayBuffer);
-            await Neutralino.filesystem.writeBinaryFile(fullPath, uint8);
-            resolve();
+            blob.arrayBuffer().then(async arrayBuffer => {
+                const uint8 = new Uint8Array(arrayBuffer);
+                await Neutralino.filesystem.writeBinaryFile(fullPath, uint8);
+                const objectUrl = cache.get(fullPath);
+                cache.set(fullPath, localPath);
+                setTimeout(() => URL.revokeObjectURL(objectUrl), 500);
+            });
+            const objectUrl = URL.createObjectURL(blob);
+            objectUrlMap.set(objectUrl, localPath);
+            cache.set(fullPath, objectUrl);
+            resolve(objectUrl);
         }, CANVAS_TYPE, CANVAS_QUALITY);
     });
 }
@@ -69,25 +77,25 @@ export function cacheImages(coreFunction, numImageArgs, shortcut = false) {
     return async function (...args) {
         if (shortcut && !args[1]) return args[0];
 
-        const key = args.join('|');
+        const key = args.map(arg => {
+            return objectUrlMap.has(arg) ? objectUrlMap.get(arg) : arg;
+        }).join('|');
         const filename = `${coreFunction.name}_${fnv1a(key)}.png`;
         const localPath = `cache/images/${filename}`;
         const fullPath = `${cachePath}/${filename}`;
         if (cache.has(fullPath))
             return cache.get(fullPath);
+        console.debug('%cCache miss %s', 'color:grey', fullPath, key);
         if (await checkFileExists(fullPath)) {
             cache.set(fullPath, localPath);
-            return fullPath;
+            return localPath;
         }
 
         const imageArgs = args.slice(0, numImageArgs);
         const otherArgs = args.slice(numImageArgs);
         const images = await Promise.all(imageArgs.map(url => loadImage(url)));
         const canvas = coreFunction(...images, ...otherArgs);
-        await writeCanvasToDisk(canvas, fullPath);
-
-        cache.set(fullPath, localPath);
-        return localPath;
+        return await writeCanvasToDisk(canvas, fullPath, cache, localPath);
     };
 }
 
