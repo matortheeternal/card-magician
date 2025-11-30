7f7534b4f5dbb91b09037230a98c1b04bfaaf3fb
Mator <why101@msn.com>
2025-11-30 15:49:48 -0800

    refactor AutoNumberModule to use a helper function

--- modules/Magic/M15/AutoNumber/main.js
+++ modules/Magic/M15/AutoNumber/main.js
@@ -1,12 +1,14 @@
 export default class AutoNumberModule extends CardMagicianModule {
+    updateNumbering() {
+        const set = this.getActiveSet();
+        const game = this.getActiveGame();
+        game.autoNumberCards(set);
+    }
+
     bind(card, watch) {
         watch(
-            () => [card.name, card.color, card.colorIdentity, card.superType, card.subType],
-            () => {
-                const set = this.getActiveSet();
-                const game = this.getActiveGame();
-                game.autoNumberCards(set);
-            }
+            () => [card.name, card.color, card.colorIdentity, card.superType],
+            () => this.updateNumbering()
         )
     }
 }
